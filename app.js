f8878c7c98b925b6bb7dcb8c7630fe29c7622d61
Agamjyot Singh <80823915+agamjyotsingh120@users.noreply.github.com>
2023-11-25 16:09:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let person = {
+    name : 'John',
+    age :'30',
+    city :'New York'}
+    delete person.age;
+    person.job = "Engineer";
+    person.city = "San Francisco"
+console.log(person);
